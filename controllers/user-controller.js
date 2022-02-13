0a9b8a0db6b5312ebc2cb056c44d448f60883ce8
nathan <nathan_santos@usp.br>
2022-02-12 23:30:44 -0300

    adicao de metodo post para cadastro de usuario

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,6 @@
 const mysql = require('../mysql');
+const bcrypt = require('bcrypt');
+const generateID = require('../functions').generateID;
 
 // GETS
 exports.getUser = async (req, res, next) => {
@@ -25,4 +27,50 @@ exports.getUser = async (req, res, next) => {
     return res.status(500).send(error);
   }
 };
+
+// POSTS
+exports.signUp = async (req, res, next) => {
+  try {
+    const newUser = {
+      id: generateID(),
+      email: req.body.email,
+      password: req.body.password,
+    }
+
+    if (!newUser.email)
+      return res.status(500).send({ error: "Email não foi fornecido" });
+
+    if (!newUser.password)
+      return res.status(500).send({ error: "Senha não foi fornecida" });
+
+    // Verificar se email/id já foi cadastrado
+    const getUserQuery = 'SELECT id, email FROM user WHERE id = ? or email = ?';
+    const getUserResult = await mysql.execute(getUserQuery, [newUser.id, newUser.email]);
+
+    if (getUserResult) {
+      if (getUserResult[0].email !== newUser.email)
+        return res.status(500).send({ error: "Erro inesperado, por favor tente novamente" });
+      else
+        return res.status(500).send({ error: 'Usuário já cadastrado no sistema '});
+    }
+    
+    // Cadastro do usuário com senha criptografada  
+    const cryptPassword = await bcrypt.hash(newUser.password, 11);
+    const createUserQuery = 'INSERT INTO user (id, email, password) VALUES (?, ?, ?)';
+    await mysql.execute(createUserQuery, [newUser.id, newUser.email, cryptPassword]);
+
+    const response = {
+      mensagem: "Usuário cadastrado com sucesso", 
+      newUser: {
+        id: newUser.id,
+        email: newUser.email,
+      }
+    }
+
+    return res.status(200).send(response);
+
+  } catch(error) {
+    return res.status(500).send(error);
+  }
+}
 
--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,6 @@ const userController = require('../controllers/user-controller');
 
 route.get('/', userController.getUser);
 
+route.post('/sign-up', userController.signUp);
+
 module.exports = route;
