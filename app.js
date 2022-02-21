ce9f2b14f9b13ffbd3cb1a4e97898c7880cea598
nathan <nathan_santos@usp.br>
2022-02-21 01:05:15 -0300

    adicionando config de CORS e body-parser

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 const userRoute = require('./routes/user');
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+});
 
 app.use('/user', userRoute);
 
